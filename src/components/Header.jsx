bd4ab4d435d06c4fdfcf024f808189507855afea
GregoireGeoffroy <thegathering92@gmail.com>
2024-10-08 16:51:06 +0300

    Add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="../images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">
+            Made by <a href="https://github.com/GregoireGeoffroy" target="_blank" rel="noopener noreferrer">Greg</a>
+            </h4>
+
+        </header>
+    )
+}
